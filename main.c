dc22d932d4a5ba48699acf863ae1fc89c0ae638b
Ravodax <kilodn84@gmail.com>
2026-05-23 02:40:02 +0700

    f

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 #define MAX 100
-#define EPS 0.000000001
+#define EPS 0.0001
+#define MAX_ITER 1000
 
 double my_abs(double x) {
     return x < 0 ? -x : x;
@@ -9,74 +10,73 @@ double my_abs(double x) {
 
 int main() {
     int n;
-    double a[MAX][MAX + 1];
+    double a[MAX][MAX];
+    double b[MAX];
     double x[MAX];
+    double x_new[MAX];
 
     printf("Введите количество неизвестных: ");
     scanf("%d", &n);
 
-    printf("Введите расширенную матрицу системы:\n");
-
+    printf("Введите матрицу коэффициентов:\n");
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= n; j++) {
+        for (int j = 0; j < n; j++) {
             scanf("%lf", &a[i][j]);
         }
     }
 
-    // Модифицированный метод Гаусса
-    for (int k = 0; k < n; k++) {
-        int maxRow = k;
-
-        // Выбор главного элемента по столбцу
-        for (int i = k + 1; i < n; i++) {
-            if (my_abs(a[i][k]) > my_abs(a[maxRow][k])) {
-                maxRow = i;
-            }
-        }
-
-        if (my_abs(a[maxRow][k]) < EPS) {
-            printf("Система не имеет единственного решения.\n");
-            return 0;
-        }
-
-        // Перестановка строк
-        if (maxRow != k) {
-            for (int j = k; j <= n; j++) {
-                double temp = a[k][j];
-                a[k][j] = a[maxRow][j];
-                a[maxRow][j] = temp;
-            }
-        }
-
-        // Нормировка ведущей строки
-        double div = a[k][k];
-        for (int j = k; j <= n; j++) {
-            a[k][j] /= div;
-        }
-
-        // Обнуление элементов ниже ведущего
-        for (int i = k + 1; i < n; i++) {
-            double factor = a[i][k];
-
-            for (int j = k; j <= n; j++) {
-                a[i][j] -= factor * a[k][j];
-            }
-        }
+    printf("Введите свободные члены:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%lf", &b[i]);
     }
 
-    // Обратный ход
-    for (int i = n - 1; i >= 0; i--) {
-        x[i] = a[i][n];
+    // Начальное приближение
+    for (int i = 0; i < n; i++) {
+        x[i] = 0;
+    }
 
-        for (int j = i + 1; j < n; j++) {
-            x[i] -= a[i][j] * x[j];
+    int iter = 0;
+
+    while (iter < MAX_ITER) {
+        double max_diff = 0;
+
+        for (int i = 0; i < n; i++) {
+            double sum = b[i];
+
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    sum -= a[i][j] * x[j];
+                }
+            }
+
+            x_new[i] = sum / a[i][i];
+
+            double diff = my_abs(x_new[i] - x[i]);
+
+            if (diff > max_diff) {
+                max_diff = diff;
+            }
+        }
+
+        // Обновление значений
+        for (int i = 0; i < n; i++) {
+            x[i] = x_new[i];
+        }
+
+        iter++;
+
+        if (max_diff < EPS) {
+            break;
         }
     }
 
     printf("\nРешение системы:\n");
+
     for (int i = 0; i < n; i++) {
         printf("x%d = %.6lf\n", i + 1, x[i]);
     }
+
+    printf("Количество итераций: %d\n", iter);
 
     return 0;
 }
