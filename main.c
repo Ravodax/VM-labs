6f8628df5cbf6e0fbb62d4b830339e8d901c1e80
Ravodax <kilodn84@gmail.com>
2026-05-23 02:35:47 +0700

    g

--- main.c
+++ main.c
@@ -23,11 +23,11 @@ int main() {
         }
     }
 
-    // Прямой ход метода Гаусса
+    // Модифицированный метод Гаусса
     for (int k = 0; k < n; k++) {
         int maxRow = k;
 
-        // Поиск главного элемента
+        // Выбор главного элемента по столбцу
         for (int i = k + 1; i < n; i++) {
             if (my_abs(a[i][k]) > my_abs(a[maxRow][k])) {
                 maxRow = i;
@@ -39,16 +39,24 @@ int main() {
             return 0;
         }
 
-        // Обмен строк
-        for (int j = k; j <= n; j++) {
-            double temp = a[k][j];
-            a[k][j] = a[maxRow][j];
-            a[maxRow][j] = temp;
+        // Перестановка строк
+        if (maxRow != k) {
+            for (int j = k; j <= n; j++) {
+                double temp = a[k][j];
+                a[k][j] = a[maxRow][j];
+                a[maxRow][j] = temp;
+            }
         }
 
-        // Обнуление элементов ниже главного
+        // Нормировка ведущей строки
+        double div = a[k][k];
+        for (int j = k; j <= n; j++) {
+            a[k][j] /= div;
+        }
+
+        // Обнуление элементов ниже ведущего
         for (int i = k + 1; i < n; i++) {
-            double factor = a[i][k] / a[k][k];
+            double factor = a[i][k];
 
             for (int j = k; j <= n; j++) {
                 a[i][j] -= factor * a[k][j];
@@ -63,12 +71,9 @@ int main() {
         for (int j = i + 1; j < n; j++) {
             x[i] -= a[i][j] * x[j];
         }
-
-        x[i] /= a[i][i];
     }
 
     printf("\nРешение системы:\n");
-
     for (int i = 0; i < n; i++) {
         printf("x%d = %.6lf\n", i + 1, x[i]);
     }
